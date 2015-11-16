cd27bf1ab40e57c9b78c9039987e31dead7b2176
Rich E <rich.eakin@gmail.com>
2015-11-16 04:27:52 -0500

    initial test with BeeThree working

--- test/StkTest/src/StkTestApp.cpp
+++ test/StkTest/src/StkTestApp.cpp
@@ -1,9 +1,11 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/Log.h"
 
 #include "cinder/audio/audio.h"
 #include "stk/include/Stk.h"
+#include "stk/include/BeeThree.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -21,18 +23,30 @@ protected:
 	void initialize() override
 	{
 		stk::Stk::setSampleRate( getSampleRate() );
+
+		CI_LOG_I( "num channels: " << getNumChannels() );
 	}
 
 	void process( ci::audio::Buffer *buffer ) override
 	{
+		stk::StkFrames synthFrames( buffer->getNumFrames(), buffer->getNumChannels() );
+		mSynth.tick( synthFrames );
 
+		float *data = buffer->getData();
+		for( size_t i = 0; i < buffer->getNumFrames(); i++ ) {
+			data[i] = synthFrames( i, 0 );
+		}
 	}
+
+public:
+	stk::BeeThree mSynth;
 };
 
 class StkTestApp : public App {
   public:
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
+	void mouseUp( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
 
@@ -42,15 +56,25 @@ class StkTestApp : public App {
 
 void StkTestApp::setup()
 {
+	stk::Stk::setRawwavePath( "/Users/r/code/cinder/rte/Cinder-Stk/lib/stk/rawwaves" );
+
 	auto ctx = audio::master();
 	mStkNode = ctx->makeNode<StkTestNode>();
 	mGain = ctx->makeNode<audio::GainNode>( 0.5f );
 
 	mStkNode >> mGain >> ctx->getOutput();
+
+	ctx->enable();
 }
 
 void StkTestApp::mouseDown( MouseEvent event )
 {
+	mStkNode->mSynth.noteOn( event.getY(), 0.5f );
+}
+
+void StkTestApp::mouseUp( MouseEvent event )
+{
+	mStkNode->mSynth.noteOff( 0.5 );
 }
 
 void StkTestApp::update()
