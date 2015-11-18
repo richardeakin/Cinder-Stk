eae1e30a0b59bf2d5eb36d67bf6b240dcdabe391
Rich E <rich.eakin@gmail.com>
2015-11-17 23:28:00 -0500

    fleshing out test

--- test/StkTest/src/StkTestApp.cpp
+++ test/StkTest/src/StkTestApp.cpp
@@ -2,10 +2,15 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "cinder/Log.h"
+#include "cinder/params/Params.h"
 
 #include "cinder/audio/audio.h"
 #include "stk/include/Stk.h"
-#include "stk/include/BeeThree.h"
+//#include "stk/include/BeeThree.h"
+#include "stk/include/Rhodey.h"
+
+const float MAX_PITCH_MIDI	= 80;
+const float	MIN_PITCH_MIDI	= 40;
 
 using namespace ci;
 using namespace ci::app;
@@ -13,6 +18,7 @@ using namespace std;
 
 typedef std::shared_ptr<class StkTestNode> StkTestNodeRef;
 
+// TODO: make this a templated type on different Stk types (Instrment, Filter, etc)
 class StkTestNode : public ci::audio::Node {
 public:
 	StkTestNode()
@@ -22,11 +28,12 @@ public:
 protected:
 	void initialize() override
 	{
-		stk::Stk::setSampleRate( getSampleRate() );
 
 		CI_LOG_I( "num channels: " << getNumChannels() );
 	}
 
+	// TODO NEXT: try out NRev, FreeVerb, and FCRev
+	
 	void process( ci::audio::Buffer *buffer ) override
 	{
 		stk::StkFrames synthFrames( buffer->getNumFrames(), buffer->getNumChannels() );
@@ -39,37 +46,69 @@ protected:
 	}
 
 public:
-	stk::BeeThree mSynth;
+	stk::Rhodey mSynth;
 };
 
 class StkTestApp : public App {
   public:
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
+	void mouseDrag( MouseEvent event ) override;
 	void mouseUp( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
 
+	void setupParams();
+	void makeNote( const vec2 &pos );
+	float quantizePitch( const vec2 &pos );
+
+
 	ci::audio::GainNodeRef	mGain;
 	StkTestNodeRef			mStkNode;
+
+	params::InterfaceGlRef	mParams;
 };
 
 void StkTestApp::setup()
 {
+	auto ctx = audio::master();
+
+	stk::Stk::setSampleRate( ctx->getSampleRate() );
+
+	// TODO: make method that sets this using a variety of search locations
+	// - relative to source code using __FILE__
+	// - rawwaves file in assets dir
 	stk::Stk::setRawwavePath( "/Users/r/code/cinder/rte/Cinder-Stk/lib/stk/rawwaves" );
 
-	auto ctx = audio::master();
 	mStkNode = ctx->makeNode<StkTestNode>();
-	mGain = ctx->makeNode<audio::GainNode>( 0.5f );
+	mGain = ctx->makeNode<audio::GainNode>( 0.75f );
 
 	mStkNode >> mGain >> ctx->getOutput();
 
 	ctx->enable();
+
+	setupParams();
+}
+
+void StkTestApp::setupParams()
+{
+	mParams = params::InterfaceGl::create( getWindow(), "Stk params", toPixels( ivec2( 200, 300 ) ) );
+
+	mParams->addParam<float>( "master gain",
+					  [this]( float value ) { mGain->setValue( value ); },
+					  [this] { return mGain->getValue(); }
+	).min( 0.0f ).max( 1.0f ).step( 0.05f );
+
 }
 
 void StkTestApp::mouseDown( MouseEvent event )
 {
-	mStkNode->mSynth.noteOn( event.getY(), 0.5f );
+	makeNote( event.getPos() );
+}
+
+void StkTestApp::mouseDrag( MouseEvent event )
+{
+	makeNote( event.getPos() );
 }
 
 void StkTestApp::mouseUp( MouseEvent event )
@@ -83,7 +122,41 @@ void StkTestApp::update()
 
 void StkTestApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear();
+
+	mParams->draw();
+}
+
+void StkTestApp::makeNote( const vec2 &pos )
+{
+	float freq = quantizePitch( pos );
+	float gain = 1.0f - pos.y / (float)getWindowHeight();
+
+	mStkNode->mSynth.noteOn( freq, gain );
+}
+
+// returns a quantized pitch (in hertz) within the minor scale
+float StkTestApp::quantizePitch( const vec2 &pos )
+{
+	const size_t scaleLength = 7;
+	float scale[scaleLength] = { 0, 2, 3, 5, 7, 8, 10 };
+
+	int pitchMidi = lroundf( lmap( pos.x, 0.0f, (float)getWindowWidth(), MIN_PITCH_MIDI, MAX_PITCH_MIDI ) );
+
+	bool quantized = false;
+	while( ! quantized ) {
+		int note = pitchMidi % 12;
+		for( size_t i = 0; i < scaleLength; i++ ) {
+			if( note == scale[i] ) {
+				quantized = true;
+				break;
+			}
+		}
+		if( ! quantized )
+			pitchMidi--;
+	}
+
+	return audio::midiToFreq( pitchMidi );
 }
 
 CINDER_APP( StkTestApp, RendererGl )
